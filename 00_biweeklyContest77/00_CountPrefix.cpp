964a71e23c63182d405f12e72827cbc2cb32884c
shubhansu-kr <shubhansu2021@gmail.com>
2022-04-30 20:06:43 +0530

    Wrong solution

---
+++ 00_biweeklyContest77/00_CountPrefix.cpp
@@ -0,0 +1,46 @@
+// 6051. Count Prefixes of a Given String
+
+// You are given a string array words and a string s, 
+// where words[i] and s comprise only of lowercase English letters.
+
+// Return the number of strings in words that are a prefix of s.
+
+// A prefix of a string is a substring that occurs at the 
+// beginning of the string. A substring is a contiguous sequence 
+// of characters within a string.
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+    // Wrong solution -> count prefix , not substring
+public:
+    int countPrefixes(vector<string>& words, string s) {
+        int count = 0 ;
+        for(auto it: words) {
+            if (s.find(it) != string::npos){
+                ++count; 
+            }
+        }
+        return count ;
+    }
+};
+
+class Solution {
+    // Wrong solution -> count prefix , not substring
+public:
+    int countPrefixes(vector<string>& words, string s) {
+        int count = 0 ;
+        for(auto it: words) {
+            if (s.find(it) != string::npos){
+                ++count; 
+            }
+        }
+        return count ;
+    }
+};
+
+int main () {
+    
+    return 0;
+}
