b5d8806f9f2f26c115e910010cfad273d783b20a
shubhansu-kr <shubhansu2021@gmail.com>
2022-04-30 20:42:35 +0530

    Unsolved

---
+++ 00_biweeklyContest77/02_CountUngardedCells.cpp
@@ -0,0 +1,28 @@
+// 6053. Count Unguarded Cells in the Grid
+
+// You are given two integers m and n representing a 0-indexed m x n 
+// grid. You are also given two 2D integer arrays guards and walls where 
+// guards[i] = [rowi, coli] and walls[j] = [rowj, colj] represent the positions 
+// of the ith guard and jth wall respectively.
+
+// A guard can see every cell in the four cardinal directions 
+// (north, east, south, or west) starting from their position unless obstructed 
+// by a wall or another guard. A cell is guarded if there is at least one guard 
+// that can see it.
+
+// Return the number of unoccupied cells that are not guarded.
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
+        
+    }
+};
+
+int main () {
+    
+    return 0;
+}
