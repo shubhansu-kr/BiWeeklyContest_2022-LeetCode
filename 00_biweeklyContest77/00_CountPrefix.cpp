bbacc3c960af2888ca120e8767db3f78cccb5572
shubhansu-kr <shubhansu2021@gmail.com>
2022-04-30 20:09:58 +0530

    COunt prefix

--- 00_biweeklyContest77/00_CountPrefix.cpp
+++ 00_biweeklyContest77/00_CountPrefix.cpp
@@ -1,46 +1,69 @@
 // 6051. Count Prefixes of a Given String
 
-// You are given a string array words and a string s, 
+// You are given a string array words and a string s,
 // where words[i] and s comprise only of lowercase English letters.
 
 // Return the number of strings in words that are a prefix of s.
 
-// A prefix of a string is a substring that occurs at the 
-// beginning of the string. A substring is a contiguous sequence 
+// A prefix of a string is a substring that occurs at the
+// beginning of the string. A substring is a contiguous sequence
 // of characters within a string.
 
 #include <bits/stdc++.h>
-using namespace std ;
+using namespace std;
 
-class Solution {
-    // Wrong solution -> count prefix , not substring
+class Solution
+{
 public:
-    int countPrefixes(vector<string>& words, string s) {
-        int count = 0 ;
-        for(auto it: words) {
-            if (s.find(it) != string::npos){
-                ++count; 
+    bool isValid(string a, string b)
+    {
+        if (a.length() >= b.length())
+        {
+            return a == b;
+        }
+        for (int i = 0; i < a.size(); i++)
+        {
+            if (a[i] != b[i])
+            {
+                return false;
             }
         }
-        return count ;
+        return true;
+    }
+    int countPrefixes(vector<string> &words, string s)
+    {
+        int count = 0;
+        for (auto it : words)
+        {
+            if (isValid(it, s))
+            {
+                ++count;
+            }
+        }
+        return count;
     }
 };
 
-class Solution {
+class Solution
+{
     // Wrong solution -> count prefix , not substring
 public:
-    int countPrefixes(vector<string>& words, string s) {
-        int count = 0 ;
-        for(auto it: words) {
-            if (s.find(it) != string::npos){
-                ++count; 
+    int countPrefixes(vector<string> &words, string s)
+    {
+        int count = 0;
+        for (auto it : words)
+        {
+            if (s.find(it) != string::npos)
+            {
+                ++count;
             }
         }
-        return count ;
+        return count;
     }
 };
 
-int main () {
-    
+int main()
+{
+
     return 0;
 }
