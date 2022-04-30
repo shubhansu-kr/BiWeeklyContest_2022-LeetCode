234b7dd5cce975949d301fbaa627627d0d01cb75
shubhansu-kr <shubhansu2021@gmail.com>
2022-04-30 20:34:18 +0530

    Wrong answer

--- 00_biweeklyContest77/01_MinimunAverageDistance.cpp
+++ 00_biweeklyContest77/01_MinimunAverageDistance.cpp
@@ -5,7 +5,38 @@ using namespace std;
 
 class Solution
 {
-    // Wrong answer : return the min. index no the min val 
+public:
+    int minimumAverageDifference(vector<int> &nums)
+    {
+        int sum = 0, n = nums.size();
+        if (n < 2)
+        {
+            return 0;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            sum += nums[i];
+        }
+        int minD = INT_MAX, currentSum = 0, minIndex;
+        for (int i = 0; i < n - 1; i++)
+        {
+            currentSum += nums[i];
+            int temp = abs(currentSum / (i + 1) - (sum - currentSum) / (n - i - 1));
+            if (temp < minD) {
+                minD = temp;
+                minIndex = i;
+            }
+        }
+        if (minD > abs(sum/n)) {
+            return n ;
+        }
+        return minIndex;
+    }
+};
+
+class Solution
+{
+    // Wrong answer : return the min. index no the min val
 public:
     int minimumAverageDifference(vector<int> &nums)
     {
