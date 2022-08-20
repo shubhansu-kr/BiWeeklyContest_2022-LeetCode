51026e09815c6996ce40500b4a011faa9e66439f
shubhansu-kr <shubhansu2021@gmail.com>
2022-08-20 21:08:44 +0530

    unsolved

---
+++ 07_BiweeklyContest85/03_MaxSegmentSum.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/contest/biweekly-contest-85/problems/maximum-segment-sum-after-removals/
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+    // Unsolved
+public:
+    vector<long long> maximumSegmentSum(vector<int>& nums, vector<int>& removeQueries) {
+        
+    }
+};
+
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
